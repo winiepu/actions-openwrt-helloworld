b6fa822ce3229716619d94de4788d33e54d03a60
winiepu <chenpuias@gmail.com>
2022-11-15 20:11:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,6 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo 'src-git smartdns https://github.com/pymumu/openwrt-smartdns' >>feeds.conf.default
-#echo 'src-git smartdns https://github.com/pymumu/luci-app-smartdns' >>feeds.conf.default
+echo 'src-git smartdns https://github.com/pymumu/luci-app-smartdns' >>feeds.conf.default
 # Lienol/openwrt-package
 #sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
